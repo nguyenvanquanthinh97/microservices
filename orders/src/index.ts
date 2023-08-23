16f7d4aaff8f1e6508050611801a624527d87f4b
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2023-08-23 13:37:14 +0700

    add order startup message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+  console.log("Order starting up...");
   try {
     if (!process.env.JWT_KEY) {
       throw new Error("JWT_KEY must be defined");
